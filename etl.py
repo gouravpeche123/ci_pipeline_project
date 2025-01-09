10a1bfee3bdf1a8d8f0a8ced74a71088c0e0272d
gourav <pechegaurav@gmail.com>
2025-01-10 00:22:10 +0530

    file added

--- etl.py
+++ etl.py
@@ -1,13 +1,11 @@
 
 import pandas as pd
 import streamlit as st
-st.write("welcome to avd to learn bigdata during data engineering course 2025")
-st.write("here we are learning data engineering course completely within the 3 months")
 data = {
     "Task":["Extract","Transform","Load"],
     "Status":["Completed","Inprogess","Pending"]
 }
 df = pd.DataFrame(data)
 st.write("ETL Pipeline Status",df)
-st.write("thank you")
+
 
