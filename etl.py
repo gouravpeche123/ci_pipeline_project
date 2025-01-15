23fbe511c7dc1e3eef90f3a5adf2ba8068fba1a5
gourav <pechegaurav@gmail.com>
2025-01-15 20:12:09 +0530

    file added

--- etl.py
+++ etl.py
@@ -1,7 +1,7 @@
 
 import pandas as pd
 import streamlit as st
-st.write("welcome to AVD 2025 for Data Engineering")
+st.write("welcome to AVD 2025 for Data Engineering now we are learning azure pyspark here")
 data = {
     "Task":["Extract","Transform","Load"],
     "Status":["Completed","Inprogess","Pending"]
